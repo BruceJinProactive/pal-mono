4e3d052e60ea14e6be73dd0dbc6ff93e87c27cb6
Chuan <chuan@proactiveailab.com>
2024-05-01 20:27:40 -0700

    openai model strings

--- ai/settings.py
+++ ai/settings.py
@@ -7,9 +7,9 @@ class AISettings(BaseSettings):
     Reference: https://pydantic-docs.helpmanual.io/usage/settings/
     """
 
-    gpt_4: str = "gpt-4-1106-preview"
-    gpt_4_vision: str = "gpt-4-vision-preview"
-    gpt_3_5: str = "gpt-3.5-turbo-1106"
+    gpt_4: str = "gpt-4-turbo"
+    gpt_4_vision: str = "gpt-4-turbo"
+    gpt_3_5: str = "gpt-3.5-turbo"
     embedding_model: str = "text-embedding-3-small"
     default_max_tokens: int = 1024
     default_temperature: float = 0
