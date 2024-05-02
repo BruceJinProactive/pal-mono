91bef3cefaa0b53059e9430cc43132585e7c591d
Chuan <chuan@proactiveailab.com>
2024-05-01 17:17:51 -0700

    coffee assistant

---
+++ ai/assistants/coffee.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+from phi.assistant import Assistant
+from phi.llm.openai import OpenAIChat
+from phi.llm.openai.like import OpenAILike
+
+from ai.settings import ai_settings
+from ai.storage import pdf_assistant_storage
+from ai.knowledge_base import pdf_knowledge_base
+
+
+def get_coffee_assistant(
+    run_id: Optional[str] = None,
+    user_id: Optional[str] = None,
+    debug_mode: bool = False,
+) -> Assistant:
+    """Get an Autonomous Assistant with a coffee knowledge base."""
+
+    return Assistant(
+        name="coffee_assistant",
+        run_id=run_id,
+        user_id=user_id,
+        llm=OpenAIChat(
+            model="gpt-3.5-turbo",
+            max_tokens=ai_settings.default_max_tokens,
+            temperature=ai_settings.default_temperature,
+        ),
+        storage=pdf_assistant_storage,
+        knowledge_base=pdf_knowledge_base,
+        # Enable monitoring on phidata.app
+        # monitoring=True,
+        use_tools=True,
+        show_tool_calls=True,
+        debug_mode=debug_mode,
+        description="You are a helpful assistant named 'Max' designed to answer questions about Max's Coffee Shop.",
+        extra_instructions=[
+            "Keep your answers under 5 sentences.",
+        ],
+        assistant_data={"assistant_type": "autonomous"},
+    )
--- app/pages/Coffee_Assistant.py
+++ app/pages/Coffee_Assistant.py
@@ -11,8 +11,7 @@ from phi.tools.streamlit.components import (
     get_username_sidebar,
 )
 
-from ai.assistants.pdf_auto import get_autonomous_pdf_assistant
-from ai.assistants.pdf_rag import get_rag_pdf_assistant
+from ai.assistants.coffee import get_coffee_assistant
 from utils.log import logger
 
 from typing import List
@@ -22,10 +21,11 @@ from phi.assistant import Assistant
 
 
 st.set_page_config(
-    page_title="Max's Coffee Shop",
+    page_title="Max's Coffee",
     page_icon=":coffee:",
 )
-st.title("Max's Coffee Shop")
+st.title("Max's Coffee")
+
 
 def main() -> None:
     # Get OpenAI key from environment variable or user input
@@ -37,66 +37,70 @@ def main() -> None:
         st.sidebar.info(f":technologist: User: {username}")
     else:
         st.markdown("---")
-        st.markdown("#### :technologist: Enter a username to start. Your profile and chat history will be saved.")
+        st.markdown(
+            "#### :technologist: Enter a username to start. Your profile and chat history will be saved."
+        )
         return
 
     # Get the assistant
-    pdf_assistant: Assistant = get_autonomous_pdf_assistant(
+    coffee_assistant: Assistant = get_coffee_assistant(
         user_id=username,
         debug_mode=True,
     )
-    
-    # Get the run id
-    pdf_assistant_run_ids: List[str] = pdf_assistant.storage.get_all_run_ids(user_id=username)
-    pdf_assistant_run_id = None
-    if not pdf_assistant_run_ids or len(pdf_assistant_run_ids) == 0:
-        pdf_assistant_run_id = pdf_assistant.create_run()
-    else:
-        pdf_assistant_run_id = pdf_assistant_run_ids[0]
-    pdf_assistant.run_id = pdf_assistant_run_id
 
+    # Get the run id
+    coffee_assistant_run_ids: List[str] = coffee_assistant.storage.get_all_run_ids(
+        user_id=username)
+    coffee_assistant_run_id = None
+    if not coffee_assistant_run_ids or len(coffee_assistant_run_ids) == 0:
+        coffee_assistant_run_id = coffee_assistant.create_run()
+    else:
+        coffee_assistant_run_id = coffee_assistant_run_ids[0]
+    coffee_assistant.run_id = coffee_assistant_run_id
 
     # Create assistant run (i.e. log to database) and save run_id in session state
-    st.session_state["pdf_assistant_run_id"] = pdf_assistant.create_run()
+    st.session_state["coffee_assistant_run_id"] = coffee_assistant.create_run()
 
     # Check if knowlege base exists
-    if pdf_assistant.knowledge_base and (
+    if coffee_assistant.knowledge_base and (
         "pdf_knowledge_base_loaded" not in st.session_state
         or not st.session_state["pdf_knowledge_base_loaded"]
     ):
-        if not pdf_assistant.knowledge_base.exists():
+        if not coffee_assistant.knowledge_base.exists():
             logger.info("Knowledge base does not exist")
             loading_container = st.sidebar.info("🧠 Loading knowledge base")
-            pdf_assistant.knowledge_base.load()
+            coffee_assistant.knowledge_base.load()
             st.session_state["pdf_knowledge_base_loaded"] = True
             st.sidebar.success("Knowledge base loaded")
             loading_container.empty()
 
     # Check if knowlege base exists
-    if pdf_assistant.knowledge_base and (
+    if coffee_assistant.knowledge_base and (
         "pdf_knowledge_base_loaded" not in st.session_state
         or not st.session_state["pdf_knowledge_base_loaded"]
     ):
-        if not pdf_assistant.knowledge_base.exists():
+        if not coffee_assistant.knowledge_base.exists():
             logger.info("Knowledge base does not exist")
             loading_container = st.sidebar.info("🧠 Loading knowledge base")
-            pdf_assistant.knowledge_base.load()
+            coffee_assistant.knowledge_base.load()
             st.session_state["pdf_knowledge_base_loaded"] = True
             st.sidebar.success("Knowledge base loaded")
             loading_container.empty()
 
     # Load messages for existing assistant
-    assistant_chat_history = pdf_assistant.memory.get_chat_history()
+    assistant_chat_history = coffee_assistant.memory.get_chat_history()
     if len(assistant_chat_history) > 0:
         logger.debug("Loading chat history")
         st.session_state["messages"] = assistant_chat_history
     else:
         logger.debug("No chat history found")
-        st.session_state["messages"] = [{"role": "assistant", "content": "Ask me anything..."}]
+        st.session_state["messages"] = [
+            {"role": "assistant", "content": "Ask me anything..."}]
 
     # Prompt for user input
     if prompt := st.chat_input():
-        st.session_state["messages"].append({"role": "user", "content": prompt})
+        st.session_state["messages"].append(
+            {"role": "user", "content": prompt})
 
     # Display existing chat messages
     for message in st.session_state["messages"]:
@@ -113,36 +117,37 @@ def main() -> None:
             with st.spinner("Working..."):
                 response = ""
                 resp_container = st.empty()
-                for delta in pdf_assistant.run(question):
+                for delta in coffee_assistant.run(question):
                     response += delta  # type: ignore
                     resp_container.markdown(response)
 
-            st.session_state["messages"].append({"role": "assistant", "content": response})
+            st.session_state["messages"].append(
+                {"role": "assistant", "content": response})
 
     # if st.sidebar.button("New Run"):
     #     restart_assistant()
 
-    if pdf_assistant.knowledge_base:
+    if coffee_assistant.knowledge_base:
         if st.sidebar.button("Update Knowledge Base"):
-            pdf_assistant.knowledge_base.load(recreate=False, upsert=True)
+            coffee_assistant.knowledge_base.load(recreate=False, upsert=True)
             st.session_state["pdf_knowledge_base_loaded"] = True
             st.sidebar.success("Knowledge base updated")
 
         if st.sidebar.button("Recreate Knowledge Base"):
-            pdf_assistant.knowledge_base.load(recreate=True)
+            coffee_assistant.knowledge_base.load(recreate=True)
             st.session_state["pdf_knowledge_base_loaded"] = True
             st.sidebar.success("Knowledge base recreated")
 
         if st.sidebar.button("Clear Knowledge Base"):
-            pdf_assistant.knowledge_base.vector_db.clear()
+            coffee_assistant.knowledge_base.vector_db.clear()
             st.session_state["pdf_knowledge_base_loaded"] = False
             st.sidebar.success("Knowledge base cleared")
 
     # if st.sidebar.button("Auto Rename"):
-    #     pdf_assistant.auto_rename_run()
+    #     coffee_assistant.auto_rename_run()
 
     # Upload PDF
-    if pdf_assistant.knowledge_base:
+    if coffee_assistant.knowledge_base:
         if "file_uploader_key" not in st.session_state:
             st.session_state["file_uploader_key"] = 0
 
@@ -158,37 +163,38 @@ def main() -> None:
                 reader = PDFReader()
                 pdf_documents: List[Document] = reader.read(uploaded_file)
                 if pdf_documents:
-                    pdf_assistant.knowledge_base.load_documents(pdf_documents)
+                    coffee_assistant.knowledge_base.load_documents(
+                        pdf_documents)
                 else:
                     st.sidebar.error("Could not read PDF")
                 st.session_state[f"{pdf_name}_uploaded"] = True
             alert.empty()
 
     # # Run id selector
-    # if pdf_assistant.storage:
-    #     pdf_assistant_run_ids: List[str] = pdf_assistant.storage.get_all_run_ids(user_id=username)
-    #     new_pdf_assistant_run_id = st.sidebar.selectbox("Run ID", options=pdf_assistant_run_ids)
-    #     if st.session_state["pdf_assistant_run_id"] != new_pdf_assistant_run_id:
-    #         logger.debug(f"Loading run {new_pdf_assistant_run_id}")
-    #         if st.session_state["pdf_assistant_type"] == "Autonomous":
+    # if coffee_assistant.storage:
+    #     coffee_assistant_run_ids: List[str] = coffee_assistant.storage.get_all_run_ids(user_id=username)
+    #     new_coffee_assistant_run_id = st.sidebar.selectbox("Run ID", options=coffee_assistant_run_ids)
+    #     if st.session_state["coffee_assistant_run_id"] != new_coffee_assistant_run_id:
+    #         logger.debug(f"Loading run {new_coffee_assistant_run_id}")
+    #         if st.session_state["coffee_assistant_type"] == "Autonomous":
     #             logger.info("---*--- Loading as Autonomous Assistant ---*---")
-    #             st.session_state["pdf_assistant"] = get_autonomous_pdf_assistant(
+    #             st.session_state["coffee_assistant"] = get_autonomous_coffee_assistant(
     #                 user_id=username,
-    #                 run_id=new_pdf_assistant_run_id,
+    #                 run_id=new_coffee_assistant_run_id,
     #                 debug_mode=True,
     #             )
     #         else:
     #             logger.info("---*--- Loading as RAG Assistant ---*---")
-    #             st.session_state["pdf_assistant"] = get_rag_pdf_assistant(
+    #             st.session_state["coffee_assistant"] = get_rag_coffee_assistant(
     #                 user_id=username,
-    #                 run_id=new_pdf_assistant_run_id,
+    #                 run_id=new_coffee_assistant_run_id,
     #                 debug_mode=True,
     #             )
     #         st.rerun()
 
-    # pdf_assistant_run_name = pdf_assistant.run_name
-    # if pdf_assistant_run_name:
-    #     st.sidebar.write(f":thread: {pdf_assistant_run_name}")
+    # coffee_assistant_run_name = coffee_assistant.run_name
+    # if coffee_assistant_run_name:
+    #     st.sidebar.write(f":thread: {coffee_assistant_run_name}")
 
     # # Show reload button
     # reload_button_sidebar()
