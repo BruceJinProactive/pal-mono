deaf5b0cd916ffa15f27ca553c16fcd4b5e8a113
Chuan <kelvin@proactiveailab.com>
2024-05-10 21:58:47 -0700

    ruff check --fix

--- ai/assistants/coffee.py
+++ ai/assistants/coffee.py
@@ -4,10 +4,8 @@ from os import getenv
 import logging
 
 from phi.assistant import Assistant
-from phi.llm.openai import OpenAIChat
 from phi.llm.openai.like import OpenAILike
 
-from ai.settings import ai_settings
 from ai.storage import pdf_assistant_storage
 from ai.knowledge_base import pdf_knowledge_base
 
