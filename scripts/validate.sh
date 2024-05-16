0b39e9546414de855308ec96383c88ac7934c662
renkelvin <renkelvin@gmail.com>
2024-05-16 14:57:31 -0700

    merge format script into validate

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -17,6 +17,8 @@ source ${CURR_DIR}/_utils.sh
 main() {
   print_heading "Validating workspace..."
   print_heading "Running: ruff check ${REPO_ROOT}"
+
+  ruff format ${REPO_ROOT}
   ruff check ${REPO_ROOT}
   # print_heading "Running: mypy ${REPO_ROOT}"
   # mypy ${REPO_ROOT}
