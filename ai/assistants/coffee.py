3d4a8ba62c0288af700138f3376308d2c74f9a79
renkelvin <renkelvin@gmail.com>
2024-05-15 18:39:10 -0700

    Update coffee knowledge base and storage

--- ai/assistants/coffee.py
+++ ai/assistants/coffee.py
@@ -8,13 +8,41 @@ from phi.llm.openai.like import OpenAILike
 
 from ai.storage import pdf_assistant_storage
 from ai.knowledge_base import pdf_knowledge_base
+from phi.embedder.openai import OpenAIEmbedder
+from phi.knowledge.combined import CombinedKnowledgeBase
+from phi.knowledge.pdf import PDFUrlKnowledgeBase, PDFKnowledgeBase
+from phi.knowledge.website import WebsiteKnowledgeBase
+from phi.vectordb.pgvector import PgVector2
 
+from ai.settings import ai_settings
+from db.session import db_url
+
+from phi.storage.assistant.postgres import PgAssistantStorage
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
 requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
+
+knowledge_base = CombinedKnowledgeBase(
+    sources=[
+        PDFKnowledgeBase(path="data/coffee/pdfs"),
+    ],
+    vector_db=PgVector2(
+        db_url=db_url,
+        # Store the embeddings in ai.coffee_documents
+        collection="coffee_knowledge_base",
+        embedder=OpenAIEmbedder(model=ai_settings.embedding_model),
+    ),
+    # 2 references are added to the prompt
+    num_documents=2,
+)
+
+storage = PgAssistantStorage(
+    db_url=db_url,
+    table_name="coffee_storage",
+)
 
 
 def get_coffee_assistant(
@@ -33,8 +61,8 @@ def get_coffee_assistant(
             api_key=getenv("LEPTON_API_KEY"),
             base_url="https://kfxrnfa5-pail-test.tin.lepton.run/api/v1/",
         ),
-        storage=pdf_assistant_storage,
-        knowledge_base=pdf_knowledge_base,
+        storage=storage,
+        knowledge_base=knowledge_base,
         # Enable monitoring on phidata.app
         # monitoring=True,
         use_tools=False,
