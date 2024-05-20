6251954cdffb74cb7758e8d112ad1522440519e2
renkelvin <renkelvin@gmail.com>
2024-05-20 15:43:01 -0700

    [DB Update] Add initial accounts table

--- db/tables/__init__.py
+++ db/tables/__init__.py
@@ -1,1 +1,2 @@
 from db.tables.base import Base
+from db.tables.account import AccountsTable
---
+++ db/tables/account.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from db.tables.base import Base
+from sqlalchemy.sql.expression import text
+from sqlalchemy.types import BigInteger, DateTime, String
+
+
+class AccountsTable(Base):
+
+    __tablename__ = "accounts"
+
+    account_id: Mapped[int] = mapped_column(
+        BigInteger, primary_key=True, autoincrement=True, nullable=False, index=True
+    )
+
+    cognito_user_group_name: Mapped[str] = mapped_column(String)
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=text("now()")
+    )
+    updated_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True), onupdate=text("now()")
+    )
