0a503bb570673fb9074b6e7653e587a9569a52c0
renkelvin <renkelvin@gmail.com>
2024-05-21 00:05:01 -0700

    Retrieve account name

--- app/pages/Account_Test.py
+++ app/pages/Account_Test.py
@@ -1,6 +1,10 @@
+import json
 import os
 
+import jwt
+import requests
 import streamlit as st
+from jwt.algorithms import RSAAlgorithm
 from streamlit_cognito_auth import CognitoAuthenticator
 
 st.set_page_config(
@@ -9,16 +13,65 @@ st.set_page_config(
 )
 st.title("Account Test")
 
-aws_user_pool_id = os.environ["AWS_USER_POOL_ID"]
-aws_app_client_id = os.environ["AWS_APP_CLIENT_ID"]
-aws_app_client_secret = os.environ["AWS_APP_CLIENT_SECRET"]
+AWS_DEFAULT_REGION = os.environ["AWS_DEFAULT_REGION"]
+AWS_USER_POOL_ID = os.environ["AWS_USER_POOL_ID"]
+AWS_APP_CLIENT_ID = os.environ["AWS_APP_CLIENT_ID"]
+AWS_APP_CLIENT_SECRET = os.environ["AWS_APP_CLIENT_SECRET"]
+
+# Cognito JWKS URL
+JWKS_URL = f"https://cognito-idp.{AWS_DEFAULT_REGION}.amazonaws.com/{AWS_USER_POOL_ID}/.well-known/jwks.json"
 
 authenticator = CognitoAuthenticator(
-    pool_id=aws_user_pool_id,
-    app_client_id=aws_app_client_id,
-    app_client_secret=aws_app_client_secret,
+    pool_id=AWS_USER_POOL_ID,
+    app_client_id=AWS_APP_CLIENT_ID,
+    app_client_secret=AWS_APP_CLIENT_SECRET,
     use_cookies=False,
 )
+
+
+def logout():
+    print("Logout in example")
+    authenticator.logout()
+
+
+def get_jwks():
+    response = requests.get(JWKS_URL)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_public_key(jwks, kid):
+    for key in jwks["keys"]:
+        if key["kid"] == kid:
+            return RSAAlgorithm.from_jwk(json.dumps(key))
+    raise ValueError("Public key not found.")
+
+
+def decode_verify_jwt(token, jwks, app_client_id):
+    headers = jwt.get_unverified_header(token)
+    kid = headers["kid"]
+
+    public_key = get_public_key(jwks, kid)
+
+    try:
+        claims = jwt.decode(
+            token, public_key, algorithms=["RS256"], audience=app_client_id
+        )
+    except jwt.ExpiredSignatureError:
+        raise ValueError("Token is expired")
+    except jwt.InvalidAudienceError:
+        raise ValueError("Token was not issued for this audience")
+    except jwt.PyJWTError as e:
+        raise ValueError(f"Token verification failed: {e}")
+
+    return claims
+
+
+def parse_id_token(id_token):
+    jwks = get_jwks()
+    claims = decode_verify_jwt(id_token, jwks, AWS_APP_CLIENT_ID)
+    return claims
+
 
 is_logged_in = authenticator.login()
 if not is_logged_in:
@@ -26,13 +79,22 @@ if not is_logged_in:
 else:
     st.write("You are logged in!")
 
-    st.write(authenticator.get_username())
-    st.write(authenticator.get_credentials())
+    st.write("username: ", authenticator.get_username())
+    st.write("email: ", authenticator.get_email())
 
+    # Example usage
+    credentials = authenticator.get_credentials()
+    if credentials is not None:
+        id_token = credentials.id_token
+    else:
+        id_token = None
+    try:
+        claims = parse_id_token(id_token)
+        st.write("claims: ", json.dumps(claims, indent=4))
+        st.write("account_name: ", claims["custom:account_name"])
 
-def logout():
-    print("Logout in example")
-    authenticator.logout()
+    except Exception as e:
+        st.write(f"Error parsing ID token: {e}")
 
 
 with st.sidebar:
