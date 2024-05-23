03695d2902e582d61cfbe2130723dd630a48999a
renkelvin <renkelvin@gmail.com>
2024-05-22 20:46:17 -0700

    streamlit updates

--- app/Home.py
+++ app/Home.py
@@ -1,19 +1,37 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Proactive AI Demos",
-    page_icon=":smile:",
+    page_title="Proactive AI Console",
+    page_icon=":control-knobs:",
 )
-st.title("Welcome to Proactive AI Demos!")
 
 
-def main() -> None:
-    st.markdown("---")
+st.title("Welcome to Proactive AI!")
 
-    st.sidebar.success("Select demo from above")
+st.markdown("---")
 
 
-# if check_password():
-#     main()
+# Sidebar Footer
+footer = """
+    <style>
+    .sidebar .sidebar-content {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 100%;
+    }
+    .footer {
+        text-align: center;
+        padding: 10px 0;
+        font-size: 12px;
+        color: gray;
+    }
+    </style>
+    <div class="footer">
+        <hr>
+        <p>© 2024 Proactive AI Lab</p>
+    </div>
+    """
 
-main()
+# Injecting the footer HTML into the sidebar
+st.sidebar.markdown(footer, unsafe_allow_html=True)
