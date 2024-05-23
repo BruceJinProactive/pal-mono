89d39b2a3001861e3425d9dd323f79b942905848
renkelvin <renkelvin@gmail.com>
2024-05-23 10:48:30 -0700

    Hide footer

--- app/Home.py
+++ app/Home.py
@@ -10,27 +10,27 @@ st.title("Welcome to Proactive AI!")
 
 st.markdown("---")
 
-# Sidebar Footer
-footer = """
-    <style>
-    .sidebar .sidebar-content {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        height: 100%;
-    }
-    .footer {
-        text-align: center;
-        padding: 10px 0;
-        font-size: 12px;
-        color: gray;
-    }
-    </style>
-    <div class="footer">
-        <hr>
-        <p>© 2024 Proactive AI Lab</p>
-    </div>
-    """
+# # Sidebar Footer
+# footer = """
+#     <style>
+#     .sidebar .sidebar-content {
+#         display: flex;
+#         flex-direction: column;
+#         justify-content: space-between;
+#         height: 100%;
+#     }
+#     .footer {
+#         text-align: center;
+#         padding: 10px 0;
+#         font-size: 12px;
+#         color: gray;
+#     }
+#     </style>
+#     <div class="footer">
+#         <hr>
+#         <p>© 2024 Proactive AI Lab</p>
+#     </div>
+#     """
 
-# Injecting the footer HTML into the sidebar
-st.sidebar.markdown(footer, unsafe_allow_html=True)
+# # Injecting the footer HTML into the sidebar
+# st.sidebar.markdown(footer, unsafe_allow_html=True)
