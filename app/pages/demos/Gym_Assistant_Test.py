c888c41f7a55aa391631698ade4e8c2ecf5f43c3
renkelvin <renkelvin@gmail.com>
2024-05-24 12:12:35 -0700

    Clean gym assistant

--- app/pages/demos/Gym_Assistant_Test.py
+++ app/pages/demos/Gym_Assistant_Test.py
@@ -1,18 +1,11 @@
-import json
-import os
 from typing import List
 
-import jwt
-import requests
 import streamlit as st
-from jwt.algorithms import RSAAlgorithm
+from Auth import user
 from phi.assistant import Assistant
-from phi.tools.streamlit.components import get_openai_key_sidebar
-from streamlit_cognito_auth import CognitoAuthenticator
 
 from ai.assistants.gym_assistant import get_gym_assistant
 from utils.log import logger
-from Auth import user
 
 st.set_page_config(
     page_title="Gym Assistant",
@@ -182,95 +175,4 @@ def main() -> None:
     # reload_button_sidebar()
 
 
-AWS_REGION = os.environ["AWS_REGION"]
-AWS_USER_POOL_ID = os.environ["AWS_USER_POOL_ID"]
-AWS_APP_CLIENT_ID = os.environ["AWS_APP_CLIENT_ID"]
-AWS_APP_CLIENT_SECRET = os.environ["AWS_APP_CLIENT_SECRET"]
-
-# Cognito JWKS URL
-JWKS_URL = f"https://cognito-idp.{AWS_REGION}.amazonaws.com/{AWS_USER_POOL_ID}/.well-known/jwks.json"
-
-authenticator = CognitoAuthenticator(
-    pool_id=AWS_USER_POOL_ID,
-    app_client_id=AWS_APP_CLIENT_ID,
-    app_client_secret=AWS_APP_CLIENT_SECRET,
-    use_cookies=False,
-)
-
-
-def logout():
-    print("Logout in example")
-    authenticator.logout()
-
-
-def get_jwks():
-    response = requests.get(JWKS_URL)
-    response.raise_for_status()
-    return response.json()
-
-
-def get_public_key(jwks, kid):
-    for key in jwks["keys"]:
-        if key["kid"] == kid:
-            return RSAAlgorithm.from_jwk(json.dumps(key))
-    raise ValueError("Public key not found.")
-
-
-def decode_verify_jwt(token, jwks, app_client_id):
-    headers = jwt.get_unverified_header(token)
-    kid = headers["kid"]
-
-    public_key = get_public_key(jwks, kid)
-
-    try:
-        claims = jwt.decode(
-            token, public_key, algorithms=["RS256"], audience=app_client_id
-        )
-    except jwt.ExpiredSignatureError:
-        raise ValueError("Token is expired")
-    except jwt.InvalidAudienceError:
-        raise ValueError("Token was not issued for this audience")
-    except jwt.PyJWTError as e:
-        raise ValueError(f"Token verification failed: {e}")
-
-    return claims
-
-
-def parse_id_token(id_token):
-    jwks = get_jwks()
-    claims = decode_verify_jwt(id_token, jwks, AWS_APP_CLIENT_ID)
-    return claims
-
-
-is_logged_in = authenticator.login()
-if not is_logged_in:
-    st.stop()
-else:
-    # st.write("---")
-    # st.write("## Cognito")
-
-    # st.write("- username: ", authenticator.get_username())
-    # st.write("- email: ", authenticator.get_email())
-
-    username = authenticator.get_email()
-
-    # Example usage
-    credentials = authenticator.get_credentials()
-    if credentials is not None:
-        id_token = credentials.id_token
-    else:
-        id_token = None
-    try:
-        claims = parse_id_token(id_token)
-        # st.write("- claims: ", json.dumps(claims, indent=4))
-        # st.write("- account_name: ", claims["custom:account_name"])
-
-        main()
-
-    except Exception as e:
-        st.write(f"Error parsing ID token: {e}")
-
-
-with st.sidebar:
-    # st.text(f"Welcome,\n{authenticator.get_email()}")
-    st.button("Logout", "logout_btn", on_click=logout)
+main()
