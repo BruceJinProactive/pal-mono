d35f91a18443fdcc845e1df32e5f15b956d956b8
ilbum <39973347+ilbum@users.noreply.github.com>
2024-07-15 17:37:14 -0700

    Bug: cli_playground not running locally, missing user_id (#26)
    * Added user_id="random_string" to the get_pizza_assistant()
    
    * Comment more concise

--- playground/cli_playground.py
+++ playground/cli_playground.py
@@ -1,6 +1,7 @@
 from ai.assistants.pizza_assistant import get_pizza_assistant
 
-assistant = get_pizza_assistant(debug_mode=True)
+# The user_id can be any random string.
+assistant = get_pizza_assistant(user_id="random_string", debug_mode=True)
 
 text = input()
 while text:
