22e916529d13710825b58a2ba5348d00d630ce29
renkelvin <renkelvin@gmail.com>
2024-08-07 22:27:09 -0700

    Reapply "Connecting FastAPI to Admin (#36)" (#39)
    This reverts commit e3095f52b70789a43a67d465f39259271014b86a.

--- services/admin_service.py
+++ services/admin_service.py
@@ -1,8 +1,30 @@
+from fastapi import Depends
+from phi.assistant.run import AssistantRun
+from phi.storage.assistant.postgres import PgAssistantStorage
 from sqlalchemy.orm import Session
 
 from db.repositories.account_repository import AccountRepository
 from db.repositories.assistant_repository import AssistantRepository
 from db.repositories.project_repository import ProjectRepository
+from db.session import get_db
+from db.settings import db_settings
+
+
+class Row:
+    def __init__(self, run: AssistantRun):
+        memory_json = run.memory
+        self.user_id = memory_json.get("user_id")
+        self.created_at = run.created_at
+        self.memories = memory_json.get("memories")
+        self.chat_history = memory_json.get("chat_history")
+
+    def to_dict(self):
+        return {
+            "user_id": self.user_id,
+            "created_at": self.created_at,
+            "memories": str(self.memories),
+            "chat_history": len(self.chat_history),
+        }
 
 
 def get_account(db: Session, account_name: str):
@@ -11,8 +33,9 @@ def get_account(db: Session, account_name: str):
     return account
 
 
-def create_account_with_defaults(db: Session, account_name: str):
-
+async def create_account_with_defaults(
+    account_name: str, db: Session = Depends(get_db)
+):
     # Instantiate the repositories
     account_repository = AccountRepository(db)
     project_repository = ProjectRepository(db)
@@ -24,3 +47,22 @@ def create_account_with_defaults(db: Session, account_name: str):
     _ = assistant_repository.create_assistant(project_id=project.id)
 
     return account
+
+
+async def get_assistant_data(account_name: str, db: Session = Depends(get_db)):
+    account = get_account(db, account_name=account_name)
+    if account is None:
+        raise ValueError("Account not found")
+    if not account.projects:
+        raise ValueError("No projects found for this account")
+    project_id = account.projects[0].id
+    storage_table_name = f"project_{project_id}_storage"
+    storage = PgAssistantStorage(
+        table_name=storage_table_name,
+        db_url=db_settings.get_db_url(),
+    )
+    all_runs = storage.get_all_runs()
+    rows = []
+    for run in all_runs:
+        rows.append(run)
+    return rows
