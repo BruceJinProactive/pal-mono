16831b09c16e5b3a920373e393fa18159607781b
renkelvin <kelvin@proactiveailab.com>
2024-08-23 21:42:07 -0700

    Fix env runtime, different from stage_name (#134)

--- workspace/prd_resources.py
+++ workspace/prd_resources.py
@@ -171,7 +171,7 @@ prd_ecs_cluster = EcsCluster(
 
 # -*- Build container environment
 container_env = {
-    "RUNTIME_ENV": ws_settings.prd_env,
+    "RUNTIME_ENV": "prd",
     # Database configuration
     "DB_HOST": AwsReference(prd_db.get_db_endpoint),
     "DB_PORT": AwsReference(prd_db.get_db_port),
