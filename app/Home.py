aba46a6996c478f91638bfb3ad8acb4f5f5c5181
renkelvin <kelvin@proactiveailab.com>
2024-08-28 16:17:37 -0700

    Remove unused pages from the internal app (#146)

--- app/Home.py
+++ app/Home.py
@@ -14,7 +14,6 @@ st.markdown("---")
 def pages():
     home_page = Page("app/Home.py", "Home", "🏠")
     console_pages = [
-        Page("app/pages/console/Messages.py", "Messages", "🖥️"),
         Page("app/pages/console/Demo.py", "Demo", "💬"),
     ]
     pal_internal_pages = [
