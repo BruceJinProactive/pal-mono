0e2cd6446dfdae83a371f78f61817b6fa197e05a
TomYang-TZ <69984048+TomYang-TZ@users.noreply.github.com>
2024-09-13 09:21:20 -0400

    ask lazydog assistant to introduce himself, add new lazydog logo (#209)

--- app/demo_template.py
+++ app/demo_template.py
@@ -205,7 +205,7 @@ def messaging_ui(assistant: Assistant) -> None:
             )
         elif assistant.name == "lazydog_assistant":
             avatar = (
-                "data/lazydog/logos/lazydog_charactor.png"
+                "data/lazydog/logos/LZRLogo.jpeg"
                 if message["role"] == "assistant"
                 else None
             )
@@ -231,7 +231,7 @@ def messaging_ui(assistant: Assistant) -> None:
                 generate_response_in_ui(
                     assistant,
                     question,
-                    avatar_path="data/lazydog/logos/lazydog_charactor.png",
+                    avatar_path="data/lazydog/logos/LZRLogo.jpeg",
                 )
             else:
                 generate_response_in_ui(assistant, question)
