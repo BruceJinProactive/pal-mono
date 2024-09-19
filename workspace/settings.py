139eeec5a06518995559377abb006b122c86be30
renkelvin <kelvin@proactiveailab.com>
2024-09-18 20:27:25 -0700

    Clean up unused Jupyter files (#222)

--- workspace/settings.py
+++ workspace/settings.py
@@ -16,7 +16,6 @@ ws_settings = WorkspaceSettings(
     dev_app_enabled=True,
     dev_api_enabled=True,
     dev_db_enabled=True,
-    # dev_jupyter_enabled=True,
     # -*- Production settings
     # prd_env="lat",
     # prd_env="stg",
