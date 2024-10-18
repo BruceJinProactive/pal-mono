9c5180d79d909605a6e963a5af25969f52e9d5e3
renkelvin <kelvin@proactiveailab.com>
2024-10-17 21:06:51 -0700

    Refactor get_client_secret function to use environment variables (#317)
    # PR Title
    
    [Task Name](paste_link_to_task)
    
    ## Description
    
    <!-- Provide a brief description of what this PR does. Include any
    relevant context (videos, screenshots) or links to any related issues.
    -->
    
    ## Reviewer Notes
    
    <!-- Quick notes to let the reviewer know what to focus on. -->
    
    ## Type of Change
    
    <!-- Delete options that are not relevant. -->
    
    - [x] Feature (non-breaking change which adds
    functionality/enhancements)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Technical improvement (testing, refactoring, performance
    improvement)
    - [ ] Documentation (changes to documentation only)
    
    ## Checklist
    
    ### Basics
    
    - [ ] I have tested my code (and created tests if applicable)
    - [ ] I have performed local code validation
    - [x] I have run `./scripts/validate.sh`
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    
    ### Commenting
    
    - [ ] All functions and endpoints I modified are documented with correct
    and up-to-date Python docstrings
    - [ ] I have commented my code in hard-to-understand areas
    - [ ] I have removed all commented-out code from the files I modified
    
    <!--
    For more information about PR Templates:
    
    https://www.notion.so/proactiveailab/Developer-Experience-f73bd67919e54ba6845c59a916abb22f?pvs=4#47247e822526439ebf317debc71a6a29
    -->
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced configuration management by utilizing environment variables
    for AWS region and client secret name.
      
    - **Bug Fixes**
    - Improved security by removing hardcoded values for sensitive
    information.
    
    - **Chores**
    - Updated GitHub Actions workflow to include environment variable for
    AWS client secret name in the testing process.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- utils/secret.py
+++ utils/secret.py
@@ -1,19 +1,22 @@
 import json
+import os
 
 from boto3.session import Session
 from botocore.exceptions import ClientError
 
+AWS_REGION = os.environ["AWS_REGION"]
+AWS_CLIENT_SECRET_NAME = os.environ["AWS_CLIENT_SECRET_NAME"]
+
 
 def get_client_secret(secret_key: str) -> str:
-    secret_name = "pal-mono-lat-client-secret"
-    region_name = "us-west-1"
-
     # Create a Secrets Manager client
     session = Session()
-    client = session.client(service_name="secretsmanager", region_name=region_name)
+    client = session.client(service_name="secretsmanager", region_name=AWS_REGION)
 
     try:
-        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+        get_secret_value_response = client.get_secret_value(
+            SecretId=AWS_CLIENT_SECRET_NAME
+        )
     except ClientError as e:
         # For a list of exceptions thrown, see
         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
