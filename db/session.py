b0603e75c9c5e700068adeaf89c9b4b69e6a91f6
renkelvin <kelvin@proactiveailab.com>
2024-10-17 22:15:11 -0700

    Refactor SQLAlchemy Engine instantiation for improved connection pool management (#319)

--- db/session.py
+++ db/session.py
@@ -5,8 +5,14 @@ from db.settings import db_settings
 
 # Create SQLAlchemy Engine using a database URL
 db_url = db_settings.get_db_url()
-db_engine: Engine = create_engine(db_url, pool_pre_ping=True)
-
+db_engine: Engine = create_engine(
+    db_url,
+    pool_size=30,  # The number of connections to keep open in the pool
+    max_overflow=50,  # The maximum number of connections to allow in overflow (beyond pool_size)
+    pool_timeout=30,  # The number of seconds to wait before giving up on getting a connection from the pool
+    pool_recycle=1800,  # The number of seconds to recycle (close and reopen) connections to avoid stale connections
+    pool_pre_ping=True,  # Check the connection before using it to avoid stale connections
+)
 # Create a SessionLocal class
 # https://fastapi.tiangolo.com/tutorial/sql-databases/#create-a-sessionlocal-class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
