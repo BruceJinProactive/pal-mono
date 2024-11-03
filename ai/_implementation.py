cfb115c32c240c1a48025b95f4d2da97e0112ea9
ZachProactiveailab <zach@proactiveailab.com>
2024-11-02 19:39:05 -0700

    Unify model interface to model router (#405)
    # PR Title
    
    [Unify model in pal-mono with model
    router](https://www.notion.so/proactiveailab/Unify-all-model-in-pal-mono-to-use-model-router-1308c0822e498022a30dc0b77b6c070e)
    
    ## Description
    
    <!-- Provide a brief description of what this PR does. Include any
    relevant context (videos, screenshots) or links to any related issues.
    -->
    
    ## Reviewer Notes
    
    <!-- Quick notes to let the reviewer know what to focus on. -->
    
    ## Type of Change
    
    <!-- Delete options that are not relevant. -->
    
    - [X] Feature (non-breaking change which adds
    functionality/enhancements)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [X] Technical improvement (testing, refactoring, performance
    improvement)
    - [ ] Documentation (changes to documentation only)
    
    ## Checklist
    
    ### Basics
    
    - [X] I have tested my code (and created tests if applicable)
    - [X] I have performed local code validation
    - [x] I have run `./scripts/validate.sh`
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    
    ### Commenting
    
    - [ ] All functions and endpoints I modified are documented with correct
    and up-to-date Python docstrings
    - [ ] I have commented my code in hard-to-understand areas
    - [ ] I have removed all commented-out code from the files I modified
    
    <!--
    For more information about PR Templates:
    
    https://www.notion.so/proactiveailab/Developer-Experience-f73bd67919e54ba6845c59a916abb22f?pvs=4#47247e822526439ebf317debc71a6a29
    -->
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new model retrieval process with `get_model`, simplifying
    how models are instantiated.
    - Added a `get_client` function for creating an OpenAI client with API
    configurations.
    
    - **Bug Fixes**
    	- Removed deprecated `get_llm` function to streamline functionality.
    
    - **Documentation**
    - Updated docstrings to reflect changes in model naming conventions and
    parameters.
    
    - **Chores**
    - Updated AISettings to include new model attributes and marked old
    attributes for future removal.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- ai/_implementation.py
+++ ai/_implementation.py
@@ -3,7 +3,7 @@ from typing import Any, Dict
 from phi.assistant.assistant import Assistant
 
 from ai.knowledge import get_knowledge
-from ai.llm import OutputModel, get_llm
+from ai.llm import OutputModel, get_model
 from ai.memory import get_memory
 from ai.prompts import get_system_prompt
 from ai.storage import get_storage
@@ -18,7 +18,7 @@ def integrate_assistant(
     new_run: bool = False,
 ) -> Assistant:
     # Set up llm
-    llm = get_llm()
+    llm = get_model()
 
     # Set up memory
     memory = get_memory(account_name)
--- ai/llm/__init__.py
+++ ai/llm/__init__.py
@@ -1,20 +1,17 @@
 from . import _implementation, _settings
 
 
-def get_llm(llm_name: _implementation.LLM = _implementation.LLM.ROUTER):
+def get_model(model_name: str = _settings.ai_settings.standard):
     """
-    Get the appropriate LLM (Large Language Model) instance based on the provided LLM name.
+    Get the appropriate LLM (Large Language Model) instance based on the provided model name.
 
     Args:
-        llm_name (LLM): The name of the LLM to retrieve. Must be one of the LLM enum values.
+        model_name: The name of the model to retrieve. Must be "standard" or "mini"
 
     Returns:
-        An instance of OpenAIChat or OpenAILike configured with the appropriate settings.
-
-    Raises:
-        ValueError: If an invalid LLM name is provided.
+        An instance of OpenAILike configured with the model router settings.
     """
-    return _implementation.get_llm(llm_name)
+    return _implementation.get_model(model_name)
 
 
 def get_embedder():
@@ -27,7 +24,16 @@ def get_embedder():
     return _implementation.get_embedder()
 
 
-LLM = _implementation.LLM  # TODO: Remove this after fully migrate to model router
+def get_client():
+    """
+    Get the model client instance providied by model router.
+
+    Returns:
+        A instance of model router client with same usage as openai client
+    """
+    return _implementation.get_client()
+
+
 OutputModel = _implementation.OutputModel
 
 ai_settings = (
@@ -35,4 +41,4 @@ ai_settings = (
 )  # TODO: Remove this after fully migrate to model router
 
 
-__all__ = ["get_llm", "ai_settings"]
+__all__ = ["get_model", "ai_settings"]
