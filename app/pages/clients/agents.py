0a959ec0df4bb2cda58a06a142b7def0423bba1c
winston-pal <winston.leung@proactiveailab.com>
2024-12-11 19:36:27 -0500

    add agent config form (#522)
    Previous branch got too outdated.
    
    https://www.notion.so/proactiveailab/Editable-agent-configs-using-a-form-13d8c0822e498029b31ae24c6e52371f?pvs=4
    
    This update accounts for an arbitrary level of nesting between dicts and
    lists in the config.
    New demo:
    
    
    https://github.com/user-attachments/assets/df0bdc6b-2ef6-4413-9456-9a8ab45d61ef
    
    
    ## Checklist
    
    ### Before merging
    - [x] Test: I have tested my changes locally and verified the changes
    work as expected. [api](http://localhost:8000/docs),
    [app](http://localhost:8501/)
    ### After merging
    - [ ] Deploy: I have verified the deployment was successful. Deployment
    status is updated in
    [#devops-release-lat-pal-mono](https://proactiveailabgroup.slack.com/archives/C07H6H6MVV0).
    - [ ] Test: I have verified that the changes work as expected on `lat`
    environment.
    [api](http://pal-mono-lat-api-lb-1443082111.us-west-1.elb.amazonaws.com/docs),
    [app](http://pal-mono-lat-app-lb-1258791823.us-west-1.elb.amazonaws.com/)
    - [ ] Monitor: I have monitored the `lat` environment stay healthy for
    10 minutes after deployment. Health status is updated in
    [#devops-health-lat](https://proactiveailabgroup.slack.com/archives/C07KAU9AUBC).
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced agent configuration editing functionality with a new form for
    easier modifications.
    - Added support for editing various data types including dictionaries
    and lists.
    - Improved user instructions and navigation for modifying agent
    configurations.
    - Success messages updated to inform users about page refresh after
    configuration updates.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: ilbum <39973347+ilbum@users.noreply.github.com>

--- app/pages/clients/agents.py
+++ app/pages/clients/agents.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
@@ -11,6 +12,66 @@ from services.agent_service import get_agent, replace_agent_config
 st.title("Agents")
 
 session = get_app_db()
+
+
+def _render_agent_config_form(
+    cur_config: dict | list, new_config: dict | list, cur_path: str, key
+) -> None:
+    """
+    Displays a form for an item (dict, list, text, num) in the agent configuration.
+
+    Args:
+        cur_config (dict|list): original agent configuration
+        new_config (dict|list): new config that is being dynamically built
+        path (str): path to the item in focus. Used only as an input label
+        key (str|int): key/index to the item in focus
+
+    Returns:
+        None, modifies new_config in place
+    """
+    if not isinstance(cur_config[key], list):
+        # eliminates some redundant text
+        st.text(cur_path)
+    if isinstance(cur_config[key], dict):
+        new_config[key] = dict(cur_config[key])
+        _render_agent_config_form_dict(cur_config[key], new_config[key], cur_path)
+    elif isinstance(cur_config[key], list):
+        new_config[key] = list(cur_config[key])
+        _render_agent_config_form_list(cur_config[key], new_config[key], cur_path)
+    elif isinstance(cur_config[key], int) or isinstance(cur_config[key], float):
+        new_config[key] = st.number_input(key=cur_path, label="", value=cur_config[key])
+        st.divider()
+    else:
+        # text input
+        new_config[key] = st.text_area(key=cur_path, label="", value=cur_config[key])
+        st.divider()
+
+
+def _render_agent_config_form_dict(
+    cur_config: dict, new_config: dict, path: str
+) -> None:
+    """
+    Dictionary wrapper for _render_agent_config_form
+    """
+    keys = cur_config.keys()
+    tabs = st.tabs(list(keys))
+    for key, tab in zip(keys, tabs):
+        with tab:
+            cur_path = f"{path}.{key}" if path else key
+            new_config[key] = cur_config[key]
+            _render_agent_config_form(cur_config, new_config, cur_path, key)
+
+
+def _render_agent_config_form_list(
+    cur_config: list, new_config: list, path: str
+) -> None:
+    """
+    List wrapper for _render_agent_config_form
+    """
+    for i, _ in enumerate(cur_config):
+        cur_path = f"{path}[{i}]"
+        new_config[i] = cur_config[i]
+        _render_agent_config_form(cur_config, new_config, cur_path, i)
 
 
 def main() -> None:
@@ -38,6 +99,35 @@ def main() -> None:
         formatted_json = json.dumps(unformatted_json, indent=4, ensure_ascii=False)
         formatted_json_str = str(formatted_json)
 
+        st.write(":red[__Edit__] agent config values")
+        with st.expander("Edit Agent Config"):
+            st.write(
+                """
+                Notes:
+                - Use tabs to navigate across the config keys
+                - This can edit values, but not the overall structure. These cannot change:
+                    - Keys
+                    - Values types
+                    - Lists lengths
+                - Float input saves all digits, despite only showing 2 decimal places
+                - Scrolling on a number input can subtly increment/decrement the value
+                """
+            )
+            with st.form(key="update_agent_config_form", border=False):
+                new_config = {}
+                _render_agent_config_form_dict(unformatted_json, new_config, "")
+                submitted = st.form_submit_button("Submit")
+                if submitted:
+                    replace_agent_config(
+                        session,
+                        agent_id=agent.id,
+                        config=new_config,
+                    )
+                    st.success("Successfully updated the agent config. Rerunning...")
+                    time.sleep(1)
+                    st.rerun()
+        st.divider()
+
         st.write(":red[__Replace__] the entire agent config")
 
         with st.form(key="replace_agent_config_form"):
@@ -58,7 +148,9 @@ def main() -> None:
                         agent_id=agent.id,
                         config=replace_config_json,
                     )
-                    st.success("Successfully replaced the agent config")
+                    st.success("Successfully replaced the agent config. Rerunning...")
+                    time.sleep(1)
+                    st.rerun()
                 else:
                     st.error("Invalid JSON format")
 
