185c5597fb3fee05615503eb35135a6efc5646e7
Andy <37781802+aandyw@users.noreply.github.com>
2025-01-13 12:10:39 -0500

    Fix AWS_REGION issue in github actions (#589)
    ## Checklist
    
    ### Before merging
    - [x] Test: I have tested my changes locally and verified the changes
    work as expected. [api](http://localhost:8000/docs),
    [app](http://localhost:8501/)
    ### After merging
    - [ ] Deploy: I have verified the deployment was successful. Deployment
    status is updated in
    [#devops-release-lat-pal-mono](https://proactiveailabgroup.slack.com/archives/C07H6H6MVV0).
    - [ ] Test: I have verified that the changes work as expected on `lat`
    environment.
    [api](http://pal-mono-lat-api-lb-1443082111.us-west-1.elb.amazonaws.com/docs),
    [app](http://pal-mono-lat-app-lb-1258791823.us-west-1.elb.amazonaws.com/)
    - [ ] Monitor: I have monitored the `lat` environment stay healthy for
    10 minutes after deployment. Health status is updated in
    [#devops-health-lat](https://proactiveailabgroup.slack.com/archives/C07KAU9AUBC).
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Bug Fixes**
    - Improved error handling for AWS region configuration by providing a
    default empty string when the environment variable is not set.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- services/asset_service/_constants.py
+++ services/asset_service/_constants.py
@@ -4,4 +4,4 @@ LOCAL_BUCKET = "local-bucket"
 
 # AWS s3 bucket env vars
 AWS_ASSET_BUCKET_NAME = os.getenv("AWS_ASSET_BUCKET_NAME", default=LOCAL_BUCKET)
-AWS_REGION = os.environ["AWS_REGION"]
+AWS_REGION = os.getenv("AWS_REGION", "")
