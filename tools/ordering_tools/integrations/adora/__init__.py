3f7a6f580f9731fe2006653869912d0f411ec7ec
Bruce Jin <85118325+bruceJin0801@users.noreply.github.com>
2025-02-05 14:49:01 -0500

    PAL-1866: Pass Special Instructions (#645)
    …history
    
    ## Checklist
    
    ### Before merging
    - [x] Test: I have tested my changes locally and verified the changes
    work as expected. [api](http://localhost:8000/docs),
    [app](http://localhost:8501/)
    ### After merging
    - [ ] Deploy: I have verified the deployment was successful. Deployment
    status is updated in
    [#devops-release-lat-pal-mono](https://proactiveailabgroup.slack.com/archives/C07H6H6MVV0).
    - [ ] Test: I have verified that the changes work as expected on `lat`
    environment.
    [api](http://pal-mono-lat-api-lb-1443082111.us-west-1.elb.amazonaws.com/docs),
    [app](http://pal-mono-lat-app-lb-1258791823.us-west-1.elb.amazonaws.com/)
    - [ ] Monitor: I have monitored the `lat` environment stay healthy for
    10 minutes after deployment. Health status is updated in
    [#devops-health-lat](https://proactiveailabgroup.slack.com/archives/C07KAU9AUBC).
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enabled an optional comment input during order processing to allow
    additional contextual information.
    - Introduced functionality to extract special instructions from chat
    history, enhancing order customization.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- tools/ordering_tools/integrations/adora/__init__.py
+++ tools/ordering_tools/integrations/adora/__init__.py
@@ -364,6 +364,9 @@ class AdoraIntegration:
             bearer_token, self.store_information["store_id"], fulfillment_strategy.value
         )
 
+        # get special instructions
+        order_comment = _utils.get_special_instructions(chat_history) or ""
+
         # validate order
         logger.debug("[AdoraIntegration.place_order] Validating order...")
         validated_order = _apis.validate_order(
@@ -374,6 +377,7 @@ class AdoraIntegration:
             adora_order_type,
             consumer,
             adora_delivery_address,
+            order_comment,
         )
 
         logger.debug(
