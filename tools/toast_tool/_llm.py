57b6fc268f8644369f154dab776e773015b9d915
s-wang-la <shuo.wang@proactiveailab.com>
2025-04-13 16:13:52 -1000

    Bring in LLM from Adora (#965)
    Use the exact same LLM as Adora. Those logic should've been able to
    shared. We have planned a refactor immediately after we tested the
    feature and understand the system better. The task has been created for
    records.
    
    ## Checklist
    
    ### Before merging
    - [ ] Test: I have tested my changes locally and verified the changes
    work as expected. [api](http://localhost:8000/docs),
    [app](http://localhost:8501/)
    ### After merging
    - [ ] Deploy: I have verified the deployment was successful. Deployment
    status is updated in
    [#devops-release-lat-pal-mono](https://proactiveailabgroup.slack.com/archives/C07H6H6MVV0).
    - [ ] Test: I have verified that the changes work as expected on `lat`
    environment.
    [api](http://pal-mono-lat-api-lb-1443082111.us-west-1.elb.amazonaws.com/docs),
    [app](http://pal-mono-lat-app-lb-1258791823.us-west-1.elb.amazonaws.com/)
    - [ ] Monitor: I have monitored the `lat` environment stay healthy for
    10 minutes after deployment. Health status is updated in
    [#devops-health-lat](https://proactiveailabgroup.slack.com/archives/C07KAU9AUBC).
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Now supports dynamic output formats, letting you receive results in
    the format that best fits your needs.
    
    - **Refactor**
    - Improved the way prompts are processed and merged for a more
    consistent result.
    - Enhanced logging of inputs and responses for clearer, more transparent
    operations.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- tools/toast_tool/_llm.py
+++ tools/toast_tool/_llm.py
@@ -1,5 +1,9 @@
-from typing import TypeVar
+from typing import TypeVar, overload
 
+from agno.agent.agent import Agent
+from agno.models.groq.groq import Groq
+from ddtrace.llmobs import LLMObs
+from ddtrace.llmobs.decorators import llm
 from pydantic import BaseModel
 
 RETRIEVE_ORDER_ITEMS_SYSTEM_PROMPT = (
@@ -64,6 +68,28 @@ Please construct the structured order from the above Chat History and Menu Items
 T = TypeVar("T", bound=BaseModel)
 
 
+@overload
+def llm_call(
+    system_prompt: str,
+    prompt: str,
+    response_format: type[T],
+    name: str = "tool",
+    reasoning: bool = True,
+) -> T | None: ...
+
+
+@overload
+def llm_call(
+    system_prompt: str,
+    prompt: str,
+    response_format: None = None,
+    name: str = "tool",
+    reasoning: bool = True,
+) -> str | None: ...
+
+
+# This LLM is brought from Adora. Let's do a refactor to merge them.
+@llm(name="get_structured_outputs")
 def llm_call(
     system_prompt: str,
     prompt: str,
@@ -72,4 +98,41 @@ def llm_call(
     reasoning: bool = True,
 ) -> T | str | None:
 
-    raise NotImplementedError("")
+    model_name = (
+        "deepseek-r1-distill-qwen-32b" if reasoning else "llama-3.3-70b-versatile"
+    )
+    client = Groq(id=model_name)
+
+    if response_format:
+        system_prompt += """
+        \n
+        Structure your response as a dictionary, do not include "json" in the beginning
+        of the response.
+        """
+
+    # Deepseek models works better if everything is passed in the user prompt
+    if "deepseek" in model_name:
+        prompt = "\n\n".join([system_prompt, prompt])
+        system_prompt = ""
+
+    agent = Agent(
+        model=client,
+        agent_id=f"ordering-tools/{name}",
+        session_id="test-session",
+        add_history_to_messages=True,
+        knowledge=None,
+        response_model=response_format,
+        system_message=system_prompt,
+        num_history_responses=0,
+        search_knowledge=False,
+    )
+
+    response = agent.run(prompt).content
+
+    LLMObs.annotate(
+        input_data=prompt,
+        output_data=str(response),
+        metadata={"system_prompt": system_prompt},
+    )
+
+    return response
