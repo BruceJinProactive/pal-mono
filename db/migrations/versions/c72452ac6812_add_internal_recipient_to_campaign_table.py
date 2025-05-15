370909aeb416ee196bc44a7c5cfc043e1e5da4d5
Frankie Liu <frankie.liu@proactiveailab.com>
2025-05-15 09:57:52 -0700

    Clean up alembic db migration (#1161)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Chores**
    - Updated migration history to reference the correct preceding
    migration.
    - Removed an unused database migration script that did not perform any
    operations.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- db/migrations/versions/c72452ac6812_add_internal_recipient_to_campaign_table.py
+++ db/migrations/versions/c72452ac6812_add_internal_recipient_to_campaign_table.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "c72452ac6812"
-down_revision: Union[str, None] = "f4e911c7947a"
+down_revision: Union[str, None] = "8f55a8c5d0ad"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
